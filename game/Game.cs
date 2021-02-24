7dae89a1b0b7e5865730d2a8444560173ff85efc
Николаев Андрей <Abramson21@mail.ru>
2021-02-24 16:23:03 +0300

    Добавьте файлы проекта.

---
+++ game/Game.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    using System.Drawing;
+    class Game
+    {
+        private static BufferedGraphicsContext _context;
+
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+
+        public static int Height { get; set; }
+
+        public static BaseObject[] _objs;
+
+        static Game() { }
+
+        public static void Init(Form form)
+        {
+            Graphics g;
+
+            _context = BufferedGraphicsManager.Current;
+
+            g = form.CreateGraphics();
+
+
+            Width = form.ClientSize.Width;
+
+            Height = form.ClientSize.Height;
+
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Load();
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+
+            foreach (BaseObject obj in _objs)
+            {
+                obj.Draw();
+            }
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+            {
+                obj.Update();
+            }
+        }
+
+        public static void Load()
+        {
+            _objs = new BaseObject[60];
+            Random ran = new Random();
+            for (int i = 0; i < _objs.Length / 2; i++)
+                //_objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+                _objs[i] = new Meteors(new Point(i * 50, ran.Next(10, 800)), new Point(0, -i), new Size(30, 30));
+            for (int i = _objs.Length / 2 ; i < _objs.Length; i++)
+                _objs[i] = new Star(new Point(i * ran.Next(10, 50), i * ran.Next(1, 20)), new Point(-i, 0), new Size(5, 5));
+            //for (int i = ((_objs.Length * 2) / 3); i < _objs.Length; i++)
+            //    //_objs[i] = new Meteors(new Point(i, 0), new Point(i, -i), new Size(30, 30));
+            //    _objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+        }
+
+    }
+}
---
+++ game/Meteors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    class Meteors : BaseObject
+    {
+        public Meteors(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            SolidBrush browBrush = new SolidBrush(Color.Brown);
+            //Game.Buffer.Graphics.DrawEllipse(new Pen(Color.Brown), new Rectangle(Pos.X, Pos.Y, Pos.X + Dir.X, Pos.Y+Dir.Y));
+            Game.Buffer.Graphics.FillEllipse(browBrush, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X;
+            Pos.Y = Pos.Y - Dir.Y;
+
+            //if (Pos.Y < 0)
+            //{
+            //    Pos.Y = Game.Height;
+            //}
+
+            if (Pos.Y > Game.Height)
+            {
+                Pos.Y = 0;
+            }
+
+        }
+    }
+}
